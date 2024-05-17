34c3e48f4283b50219a1154cc9c1b723ad1f88d0
nayefradwi <naif.mazen.radwi@gmail.com>
2024-05-17 23:15:27 +0300

    modified stuff

--- lib/src/widgets/feature_builder.dart
+++ lib/src/widgets/feature_builder.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_feature_manager/src/domain/feature.dart';
 import 'package:flutter_feature_manager/src/extensions/context.dart';
 
-class FeatureBuilder<T> extends StatelessWidget {
+class FeatureBuilder<T> extends StatefulWidget {
   final String featureKey;
   final Widget Function(BuildContext context, T? value) builder;
   const FeatureBuilder({
@@ -14,10 +14,42 @@ class FeatureBuilder<T> extends StatelessWidget {
   });
 
   @override
+  State<FeatureBuilder<T>> createState() => _FeatureBuilderState<T>();
+}
+
+class _FeatureBuilderState<T> extends State<FeatureBuilder<T>> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.featureManager.addFeatureListener<T>(
+        key: widget.featureKey,
+        listener: _onFeatureChange,
+      );
+    });
+  }
+
+  void _onFeatureChange({
+    required Feature<T>? previous,
+    required Feature<T> current,
+  }) {
+    if (previous?.value != current.value) setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     final manager = context.featureManager;
-    final feature = manager.tryToGetFeature<T>(featureKey);
-    return builder(context, feature?.value);
+    final feature = manager.tryToGetFeature<T>(widget.featureKey);
+    return widget.builder(context, feature?.value);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    context.featureManager.removeFeatureListener<T>(
+      key: widget.featureKey,
+      listener: _onFeatureChange,
+    );
   }
 }
 
