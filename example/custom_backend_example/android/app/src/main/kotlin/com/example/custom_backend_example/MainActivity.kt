29ec6daea6cbdb3792534f19092cba34510f3172
nayefradwi <naif.mazen.radwi@gmail.com>
2024-05-17 18:02:17 +0300

    Added sample projects for examples

---
+++ example/custom_backend_example/android/app/src/main/kotlin/com/example/custom_backend_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.custom_backend_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ example/firebase_remote_config_example/android/app/src/main/kotlin/com/example/firebase_remote_config_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.firebase_remote_config_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
